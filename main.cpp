45b103b9551fdf132ffe7c7d72737b0bbe85ee6b
aburke1605 <aodhanburke@hotmail.com>
2025-06-14 16:48:19 +0100

    ball class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <SFML/Graphics.hpp>
 
 
+class ball {
+	private:
+		sf::CircleShape circle;
+
+	public:
+		ball() {
+			circle = sf::CircleShape(10.0);
+			circle.setPosition(5.0, 5.0);
+			circle.setFillColor(sf::Color::White);
+		}
+		const sf::Shape& get_shape_handle() {
+			return circle;
+		}
+		void render(sf::RenderWindow& window) {
+			window.draw(circle);
+		}
+};
+
 
 int main() {
 

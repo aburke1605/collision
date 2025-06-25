b2f20e1644606f34a2a703c4a688be90e9a840ce
aburke1605 <aodhanburke@hotmail.com>
2025-06-25 16:05:09 +0100

    fix object moving by using switch case

--- include/scene.h
+++ include/scene.h
@@ -13,11 +13,10 @@ class scene {
 	private:
 		std::vector<std::shared_ptr<object>> objects;
 		clk::time_point time;
-		bool interacting;
+		std::shared_ptr<object> selected_object;
 
 	public:
 		scene();
 		void parse_user_input(sf::RenderWindow&);
 		void update(sf::RenderWindow&);
-		void move_object(sf::Vector2i);
 };
--- src/scene.cpp
+++ src/scene.cpp
@@ -1,6 +1,6 @@
 #include "../include/scene.h"
 
-scene::scene(): interacting(false) {
+scene::scene() {
 	time = clk::now();
 
 	objects.push_back(std::make_shared<ball>());
@@ -13,12 +13,33 @@ void scene::parse_user_input(sf::RenderWindow& window) {
 		if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
 			window.close();
 
-		if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
-			interacting = true;
-		}
+		switch (event.type) {
+			case sf::Event::MouseButtonPressed:
+				if (event.mouseButton.button == sf::Mouse::Left) {
+					// identify clicked object
+					sf::Vector2i mouse_position = sf::Mouse::getPosition(window);
+					auto which = std::find_if(objects.begin(), objects.end(), [mouse_position](auto& it) -> bool {
+						if (
+							(float)mouse_position.x > it->get_position().get_x() &&
+							(float)mouse_position.x < it->get_position().get_x() + it->get_dimension() &&
+							(float)mouse_position.y > it->get_position().get_y() &&
+							(float)mouse_position.y < it->get_position().get_y() + it->get_dimension()
+						) return true;
+						return false;
+					});
+					if (which != objects.end()) selected_object = *which;
+					break;
+				}
 
-		else if (event.type == sf::Event::MouseButtonReleased)
-			interacting = false;
+			case sf::Event::MouseButtonReleased:
+				if (event.mouseButton.button == sf::Mouse::Left) {
+					selected_object = nullptr;
+					break;
+				}
+
+			default:
+				break;
+		}
 	}
 }
 
@@ -30,25 +51,12 @@ void scene::update(sf::RenderWindow& window) {
 	float dt = delta.count(); // in seconds
 	time = current_time;
 
-	if (interacting)
-		move_object(sf::Mouse::getPosition(window));
+	sf::Vector2i mouse_position = sf::Mouse::getPosition(window);
+	if (selected_object != nullptr) {
+		selected_object->set_position(vec<float>(mouse_position.x - selected_object->get_dimension() / 2, mouse_position.y - selected_object->get_dimension() / 2));
+		selected_object->set_velocity(vec<float>(0.0, 0.0));
+	}
 
 	for (std::vector<std::shared_ptr<object>>::iterator it = objects.begin(); it != objects.end(); it++)
 		(*it)->render(window, dt);
-}
-
-void scene::move_object(sf::Vector2i mouse_position) {
-	auto which = std::find_if(objects.begin(), objects.end(), [mouse_position](auto& it) -> bool {
-		if (
-			(float)mouse_position.x > it->get_position().get_x() &&
-			(float)mouse_position.x < it->get_position().get_x() + it->get_dimension() &&
-			(float)mouse_position.y > it->get_position().get_y() &&
-			(float)mouse_position.y < it->get_position().get_y() + it->get_dimension()
-		) return true;
-		return false;
-	});
-	if (which != objects.end()) {
-		(*which)->set_position(vec<float>(mouse_position.x - (*which)->get_dimension() / 2, mouse_position.y - (*which)->get_dimension() / 2));
-		(*which)->set_velocity(vec<float>(0.0, 0.0));
-	}
 }
